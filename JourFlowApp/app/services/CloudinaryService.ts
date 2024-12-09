f0f2e4e092e98c4c4d8ef3525924d97ee63e3b9c
NTBDuy <duyntb2001@gmail.com>
2024-12-09 12:40:30 +0700

    CloudinaryServicd

---
+++ JourFlowApp/app/services/CloudinaryService.ts
@@ -0,0 +1,41 @@
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from "@env";
+import axios, { AxiosError } from "axios";
+
+interface CloudinaryResponse {
+  secure_url: string;
+  public_id: string;
+}
+
+interface CloudinaryFormData {
+  uri: string;
+  type: string;
+  name: string;
+}
+
+export const uploadImageToCloudinary = async (
+  imageUri: string
+): Promise<string | null> => {
+  try {
+    const formData = new FormData();
+    const imageData: CloudinaryFormData = {
+      uri: imageUri,
+      type: "image/jpeg",
+      name: "upload.jpg",
+    };
+    formData.append("file", imageData as unknown as Blob);
+    formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+    const response = await axios.post<CloudinaryResponse>(
+      `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
+      formData,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+    return response.data.secure_url;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    console.error(
+      "Cloudinary upload failed:",
+      axiosError.response?.data || axiosError.message
+    );
+    return null;
+  }
+};
